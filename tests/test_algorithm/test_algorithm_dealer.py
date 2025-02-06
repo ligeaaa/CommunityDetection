713cbdb7c8065f04b03ed676a33098f2ed9fafaa
PanLianggang <1304412077@qq.com>
2025-02-06 10:47:12 +0800

    test(algorithm): add test code of louvain, sbm, spectral clustering, algorithm dealer

---
+++ tests/test_algorithm/test_algorithm_dealer.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding=utf-8
+import unittest
+
+import networkx as nx
+
+from algorithm.algorithm_dealer import AlgorithmDealer, Algorithm
+from algorithm.classic.SBM import SBM
+from algorithm.classic.louvain import Louvain
+from algorithm.classic.spectral_clustering import SpectralCluster
+
+
+class TestAlgorithmDealer(unittest.TestCase):
+
+    def testFormatResults(self):
+        raw_results = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+        format_results = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
+        self.assertCountEqual(Algorithm.format_results(raw_results), format_results)
+
+    def testEasyGraph(self):
+        # 示例输入：边的列表
+        edge_list = [
+            [1, 2],
+            [1, 3],
+            [1, 4],
+            [1, 5],
+            [2, 3],
+            [2, 4],
+            [2, 5],
+            [3, 4],
+            [3, 5],
+            [4, 5],
+            [5, 6],
+            [6, 7],
+            [6, 8],
+            [6, 9],
+            [6, 10],
+            [7, 8],
+            [7, 9],
+            [7, 10],
+            [8, 9],
+            [8, 10],
+            [9, 10],
+        ]
+        truth_table = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+
+        G = nx.Graph()
+        G.add_edges_from(edge_list)
+        algorithm_dealer = AlgorithmDealer()
+        louvain_algorithm = Louvain()
+        sbm_algorithm = SBM()
+        spectral_clustering_algorithm = SpectralCluster()
+
+        results = algorithm_dealer.run(
+            [louvain_algorithm, sbm_algorithm, spectral_clustering_algorithm],
+            G,
+            num_clusters=2,
+        )
+
+        for result in results:
+            communities = result.communities
+            self.assertCountEqual(communities, Algorithm.format_results(truth_table))
---
+++ tests/test_algorithm/test_sbm.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding=utf-8
+import unittest
+
+import networkx as nx
+
+from algorithm.algorithm_dealer import AlgorithmDealer, Algorithm
+from algorithm.classic.SBM import SBM
+
+
+class TestLouvain(unittest.TestCase):
+    def testEasyGraph(self):
+        # 示例输入：边的列表
+        edge_list = [
+            [1, 2],
+            [1, 3],
+            [1, 4],
+            [1, 5],
+            [2, 3],
+            [2, 4],
+            [2, 5],
+            [3, 4],
+            [3, 5],
+            [4, 5],
+            [5, 6],
+            [6, 7],
+            [6, 8],
+            [6, 9],
+            [6, 10],
+            [7, 8],
+            [7, 9],
+            [7, 10],
+            [8, 9],
+            [8, 10],
+            [9, 10],
+        ]
+        truth_table = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+
+        G = nx.Graph()
+        G.add_edges_from(edge_list)
+        algorithm_dealer = AlgorithmDealer()
+        sbm_algorithm = SBM()
+
+        results = algorithm_dealer.run([sbm_algorithm], G, num_clusters=2)
+        communities = results[0].communities
+
+        self.assertCountEqual(communities, Algorithm.format_results(truth_table))
